19510482d406b33d117794c2e4c74aefa3dc2f0f
Sara <sara.rosati@tiscali.it>
2025-04-19 14:39:25 +0200

    fix bg audio

--- assets/js/script.js
+++ assets/js/script.js
@@ -246,14 +246,24 @@ function loseLife() {
 startBtn.addEventListener('click', ()=>{
     startScreen.classList.add('hidden');
     gameContainer.classList.remove('hidden');
-    bgMusic.play();
+    bgMusic.volume = 0.5; 
+    bgMusic.play().catch(err => {
+        console.warn("Audio play was blocked by the browser:", err);
+    });
+    startScreen.classList.add('hidden');
+    gameContainer.classList.remove('hidden');
     startGame();
 });
 // Restart the game when the Restart button is clicked
 restartBtn.addEventListener('click',() =>{
     gameOverDisplay.classList.add('hidden');
     gameWinDisplay.classList.add('hidden');
-    bgMusic.play();
+    bgMusic.currentTime = 0;
+    bgMusic.play().catch(err => {
+        console.warn("Audio play was blocked on restart:", err);
+    });
+    gameOverDisplay.classList.add('hidden');
+    gameWinDisplay.classList.add('hidden');
     startGame();
     
 })
